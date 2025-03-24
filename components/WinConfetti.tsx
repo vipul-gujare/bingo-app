2b160a70fcba6b18afd5298abad426f01e41efa8
Vipul Gujare <vipulgujare@gmail.com>
2025-03-24 22:29:27 +0530

    Refactor WinConfetti component to adjust confetti count and origin based on screen width

--- components/WinConfetti.tsx
+++ components/WinConfetti.tsx
@@ -1,9 +1,12 @@
 import { useRef, useEffect } from "react";
 import ConfettiCannon from "react-native-confetti-cannon";
+import { Dimensions } from "react-native";
 
 interface WinConfettiProps {
   isVisible: boolean;
 }
+
+const { width } = Dimensions.get("window");
 
 export const WinConfetti = ({ isVisible }: WinConfettiProps) => {
   const confettiRef = useRef<ConfettiCannon>(null);
@@ -19,12 +22,12 @@ export const WinConfetti = ({ isVisible }: WinConfettiProps) => {
   return (
     <ConfettiCannon
       ref={confettiRef}
-      count={200}
-      origin={{ x: -10, y: 0 }}
+      count={150}
+      origin={{ x: width / 2, y: 0 }}
       autoStart={false}
       fadeOut
-      explosionSpeed={350}
-      fallSpeed={3000}
+      explosionSpeed={250}
+      fallSpeed={2500}
       colors={["#2196F3", "#4CAF50", "#FFC107", "#F44336", "#9C27B0"]}
     />
   );
